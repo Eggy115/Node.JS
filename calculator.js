70aaa939e512178a0f973836171282397989d17b
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-14 11:22:44 +0000

    Create

---
+++ calculator.js
@@ -0,0 +1,20 @@
+// Returns addition of two numbers
+add = function (a, b) {
+    return a+b;
+}; 
+ 
+// Returns difference of two numbers
+subtract = function (a, b) {
+    return a-b;
+}; 
+ 
+// Returns product of two numbers
+multiply = function (a, b) {
+    return a*b;
+};
+
+var a=10, b=5;
+ 
+console.log("Addition : "+add(a,b));
+console.log("Subtraction : "+subtract(a,b));
+console.log("Multiplication : "+multiply(a,b));
