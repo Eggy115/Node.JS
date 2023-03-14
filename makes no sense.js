e9754b00f0f6ec1dd5d0beaffde95331d998a5d0
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-14 11:16:57 +0000

    Create

---
+++ makes no sense.js
@@ -0,0 +1,10 @@
+console.log('hello world')
+console.log(2 + "2");
+console.log(2 - "2");
+console.log([] + {});
+console.log({} + []);
+console.log({} + {});
+console.log([] + []);
+console.log(0 == []);
+console.log(0 == "0");
+console.log("0" == []);
