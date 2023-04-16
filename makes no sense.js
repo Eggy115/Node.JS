bd29ce053d63a4e322e715362b33b84bfa7f0298
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-16 08:01:10 +0100

    Update makes no sense.js

--- makes no sense.js
+++ makes no sense.js
@@ -1,10 +1,18 @@
 console.log('hello world')
+
 console.log(2 + "2");
 console.log(2 - "2");
+
 console.log([] + {});
 console.log({} + []);
+
 console.log({} + {});
 console.log([] + []);
+
 console.log(0 == []);
 console.log(0 == "0");
+console.log(0 == {});
+
 console.log("0" == []);
+console.log("0" == {});
+console.log({} == []);
